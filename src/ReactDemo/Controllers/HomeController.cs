829b8af5d927e71d0af82d5fedb437dc9cbc5c75
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-01-08 05:32:00 +0800

    .

--- src/ReactDemo/Controllers/HomeController.cs
+++ src/ReactDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,41 @@ namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
         // GET: /<controller>/
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
         }
     }
 }
